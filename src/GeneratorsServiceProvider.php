3ee3b96a0d4ffc867999da824303ba6f3907b364
Cristian Tabacitu <cristitabacitu@gmail.com>
2018-02-13 19:30:39 +0000

    removed boot method from service provider

--- src/GeneratorsServiceProvider.php
+++ src/GeneratorsServiceProvider.php
@@ -19,16 +19,6 @@ class GeneratorsServiceProvider extends ServiceProvider
     ];
 
     /**
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
      * Register any package services.
      *
      * @return void
